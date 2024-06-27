46aaaa4048ec2e6d95e9ce1cf06db4fb1d1f6494
Xavier Didelot <xavier.didelot@gmail.com>
2024-06-27 10:56:34 +0100

    parallel runs

--- confounding-multi.R
+++ confounding-multi.R
@@ -1,21 +1,34 @@
 library(BactDating)
 library(ape)
-library(mlesky)
 source('simStructure.R')
+library(foreach)
 
-allres=matrix(NA,10,3)
-for (rep in 1:10) {
+cl <- parallel::makeCluster(parallel::detectCores() - 1, type = "PSOCK")
+doParallel::registerDoParallel(cl)
+
+allres <- foreach (rep = 1:100,.packages = c('ape','BactDating')) %dopar% {
   set.seed(rep)
   s=list(runif(30,2010,2010.01),runif(30,2010.1,2010.11),runif(30,2010.2,2010.21))
-  dt=simStructure(popStarts=c(2000,2000,2000),globalNeg = 30,samplingDates=s)
+  dt=simStructure(popStarts=c(2000,2000,2000),globalNeg = 10,samplingDates=s)
   dates=unname(dist.nodes(dt)[Ntip(dt)+1,1:Ntip(dt)])+dt$root.time
 
   tree=simobsphy(dt,mu=10,model='poisson')
   res=suppressWarnings(roottotip(tree,dates,showFig = F))
-  allres[rep,]=c(res$rate,res$ori-dt$root.time,res$pvalue)
+  r=bactdate(tree,dates,model='poisson',updateRoot = F)
+  return(list(seed=rep,rate=res$rate,ori=res$ori-dt$root.time,pval=res$pvalue,resbactdate=r))
 }
-w=which(allres[,3]<0.01)
+parallel::stopCluster(cl)
+save.image('confounding-multi.RData')
+
+v1=c()
+v2=c()
+for (i in 1:length(allres)) {
+  if (allres[[i]]$pval<0.01) {
+    v1=c(v1,allres[[i]]$rate)
+    v2=c(v2,allres[[i]]$ori)
+  }
+}
 par(mfrow=c(2,1))
-hist(allres[w,1],xlab='Clock rate',main='')
-hist(allres[w,2],xlab='Root date',main='')
+hist(v1,xlab='Clock rate (truth is 10)',main='',breaks=20) 
+hist(v2,xlab='Root date (truth is 0)',main='',breaks=20)
 
