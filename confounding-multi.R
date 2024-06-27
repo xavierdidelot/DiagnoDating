49ff5974151b02b2bcb646e67155ed55528ec76e
Xavier Didelot <xavier.didelot@gmail.com>
2024-06-27 13:42:44 +0100

    para

--- confounding-multi.R
+++ confounding-multi.R
@@ -2,6 +2,7 @@ library(BactDating)
 library(ape)
 source('simStructure.R')
 library(foreach)
+source('wrappers.R')
 
 cl <- parallel::makeCluster(parallel::detectCores() - 1, type = "PSOCK")
 doParallel::registerDoParallel(cl)
@@ -14,21 +15,32 @@ allres <- foreach (rep = 1:100,.packages = c('ape','BactDating')) %dopar% {
 
   tree=simobsphy(dt,mu=10,model='poisson')
   res=suppressWarnings(roottotip(tree,dates,showFig = F))
-  r=bactdate(tree,dates,model='poisson',updateRoot = F)
-  return(list(seed=rep,rate=res$rate,ori=res$ori-dt$root.time,pval=res$pvalue,resbactdate=r))
+  rbactdate=bactdate(tree,dates,model='poisson',updateRoot = F)
+  rlsd=runLSD(tree,dates,rep)
+  return(list(seed=rep,dt=dt,rate=res$rate,ori=res$ori-dt$root.time,pval=res$pvalue,resbactdate=rbactdate,lsdrate=rlsd$rate,lsdtmrca=rlsd$tmrca))
 }
 parallel::stopCluster(cl)
 save.image('confounding-multi.RData')
 
-v1=c()
-v2=c()
+rm(list=ls())
+load('confounding-multi.RData')
+v1=v2=v3=v4=v5=v6=c()
 for (i in 1:length(allres)) {
   if (allres[[i]]$pval<0.01) {
     v1=c(v1,allres[[i]]$rate)
     v2=c(v2,allres[[i]]$ori)
   }
+  rec=allres[[i]]$resbactdate$record
+  rec=rec[(nrow(rec)/2):nrow(rec),]
+  v3=c(v3,mean(rec[,'mu']))
+  v4=c(v4,allres[[i]]$resbactdate$rootdate-allres[[i]]$dt$root.time)
+  v5=c(v5,allres[[i]]$lsdrate)
+  v6=c(v6,allres[[i]]$lsdtmrca-allres[[i]]$dt$root.time)
 }
-par(mfrow=c(2,1))
+par(mfrow=c(3,2))
 hist(v1,xlab='Clock rate (truth is 10)',main='',breaks=20) 
 hist(v2,xlab='Root date (truth is 0)',main='',breaks=20)
-
+hist(v3,xlab='Clock rate (truth is 10)',main='',breaks=20) 
+hist(v4,xlab='Root date (truth is 0)',main='',breaks=20)
+hist(v5,xlab='Clock rate (truth is 10)',main='',breaks=20) 
+hist(v6,xlab='Root date (truth is 0)',main='',breaks=20)
--- wrappers.R
+++ wrappers.R
@@ -12,16 +12,16 @@ runTreeDater=function(tree,dates) {
   return(res)
 }
 
-runLSD=function(tree,dates) {
+runLSD=function(tree,dates,tag=0) {
   tre=tree
   l=max(tree$edge.length)*1000
   tre$edge.length=tre$edge.length/l
   tre$tip.label=1:Ntip(tre)
   sts=dates
   names(sts)=1:Ntip(tre)
-  write.tree(tre,'/tmp/tree.nwk')
-  write.table(sts,'/tmp/dates.csv',quote = F,col.names=length(sts))
-  o=capture.output(result <- Rlsd2::lsd2(inputTree='/tmp/tree.nwk', inputDate='/tmp/dates.csv',outFile = '/tmp/result', seqLen=l))
+  write.tree(tre,sprintf('/tmp/tree%d.nwk',tag))
+  write.table(sts,sprintf('/tmp/dates%d.csv',tag),quote = F,col.names=length(sts))
+  o=capture.output(result <- Rlsd2::lsd2(inputTree=sprintf('/tmp/tree%d.nwk',tag), inputDate=sprintf('/tmp/dates%d.csv',tag),outFile = sprintf('/tmp/result%d',tag), seqLen=l))
   res=list()
   res$rate=result$rate*l
   res$tmrca=result$tMRCA
