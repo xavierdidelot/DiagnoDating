d6f9f8ae8c65b18bbea9f6e1e4a6247351aa0c63
Xavier Didelot <xavier.didelot@gmail.com>
2024-07-02 15:50:19 +0100

    Update confounding-multi.R

--- run/confounding-multi.R
+++ run/confounding-multi.R
@@ -1,46 +1,41 @@
 library(BactDating)
 library(ape)
-source('simStructure.R')
 library(foreach)
-source('wrappers.R')
+library(confounding)
 
 cl <- parallel::makeCluster(parallel::detectCores() - 1, type = "PSOCK")
 doParallel::registerDoParallel(cl)
 
-allres <- foreach (rep = 1:100,.packages = c('ape','BactDating')) %dopar% {
+allres <- foreach (rep = 1:100,.packages = c('ape','BactDating','confounding')) %dopar% {
   set.seed(rep)
-  s=list(runif(30,2010,2010.01),runif(30,2010.1,2010.11),runif(30,2010.2,2010.21))
-  dt=simStructure(popStarts=c(2000,2000,2000),globalNeg = 10,samplingDates=s)
+  s=list(runif(30,2010,2010.1),runif(30,2010.1,2010.2),runif(30,2010.2,2010.3))
+  dt=simStructure(popStarts=c(2009,2009,2009),samplingDates=s)
   dates=unname(dist.nodes(dt)[Ntip(dt)+1,1:Ntip(dt)])+dt$root.time
 
   tree=simobsphy(dt,mu=10,model='poisson')
   res=suppressWarnings(roottotip(tree,dates,showFig = F))
   rbactdate=bactdate(tree,dates,model='poisson',updateRoot = F)
-  rlsd=runLSD(tree,dates,rep)
-  return(list(seed=rep,dt=dt,rate=res$rate,ori=res$ori-dt$root.time,pval=res$pvalue,resbactdate=rbactdate,lsdrate=rlsd$rate,lsdtmrca=rlsd$tmrca))
+  rlsd=runLSD(tree,dates)
+  rtreedater=runTreeDater(tree,dates)
+  return(list(seed=rep,dt=dt,rate=res$rate,ori=res$ori,pval=res$pvalue,rbactdate=rbactdate,rlsd=rlsd,rtreedater=rtreedater))
 }
 parallel::stopCluster(cl)
 save.image('confounding-multi.RData')
 
 rm(list=ls())
 load('confounding-multi.RData')
-v1=v2=v3=v4=v5=v6=c()
+v=matrix(NA,length(allres),8)
 for (i in 1:length(allres)) {
-  if (allres[[i]]$pval<0.01) {
-    v1=c(v1,allres[[i]]$rate)
-    v2=c(v2,allres[[i]]$ori)
-  }
-  rec=allres[[i]]$resbactdate$record
-  rec=rec[(nrow(rec)/2):nrow(rec),]
-  v3=c(v3,mean(rec[,'mu']))
-  v4=c(v4,allres[[i]]$resbactdate$rootdate-allres[[i]]$dt$root.time)
-  v5=c(v5,allres[[i]]$lsdrate)
-  v6=c(v6,allres[[i]]$lsdtmrca-allres[[i]]$dt$root.time)
+    v[i,1]=allres[[i]]$rate
+    v[i,2]=allres[[i]]$ori-allres[[i]]$dt$root.time
+    for (j in 1:3) {
+      rec=allres[[i]][[j+5]]$record
+      rec=rec[(nrow(rec)/2):nrow(rec),]
+      v[i,3+(j-1)*2. ]=mean(rec[,'mu'])
+      v[i,3+(j-1)*2+1]=allres[[i]][[j+5]]$rootdate-allres[[i]]$dt$root.time
+    }
 }
-par(mfrow=c(3,2))
-hist(v1,xlab='Clock rate (truth is 10)',main='',breaks=20) 
-hist(v2,xlab='Root date (truth is 0)',main='',breaks=20)
-hist(v3,xlab='Clock rate (truth is 10)',main='',breaks=20) 
-hist(v4,xlab='Root date (truth is 0)',main='',breaks=20)
-hist(v5,xlab='Clock rate (truth is 10)',main='',breaks=20) 
-hist(v6,xlab='Root date (truth is 0)',main='',breaks=20)
+par(mfrow=c(4,2))
+for (i in 1:8) {
+  hist(v[,i],breaks=20)
+}
