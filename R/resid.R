cf944a9f4e40e32ca2727e3ee6608e0ba4e603ba
Xavier Didelot <xavier.didelot@gmail.com>
2024-07-08 17:26:59 +0100

    residual analysis

--- R/resid.R
+++ R/resid.R
@@ -15,7 +15,10 @@ calcProbBranches = function(x,cumul=FALSE,log=FALSE) {
     probs=dpois(round(ys),xs*rate,log=log)
   } else {
     #probs=ppois(round(ys),xs*rate,log.p=log)
+    oldseed <- .Random.seed
+    set.seed(1998)
     probs=runif(length(ys),ppois(round(ys-1),xs*rate),ppois(round(ys),xs*rate))
+    .Random.seed <- oldseed
     if (log) probs=log(probs)
   }
   return(probs)
@@ -75,19 +78,31 @@ plotResid = function(x) {
   old.par=par(no.readonly = T)
   par(mfrow=c(2,2))
   hist(p,xlab='',main='Uniform pseudo-residuals',freq=F)
-  lines(c(0,1),c(1,1))
+  abline(1,0,lty=3)
 
-  plot(n,xlab='',ylab='',main='Normal pseudo-residuals',ylim=c(mi,ma))
-  abline(h=qnorm(c(0.005,0.025,0.5,0.975,0.995)))
+  o=order(x$tree$edge.length)
+  plot(n[o],xlab='Branches in increasing order of duration',ylab='',main='Normal pseudo-residuals',ylim=c(mi,ma))
+  abline(h=qnorm(c(0.005,0.025,0.5,0.975,0.995)),lty=3)
 
   hist(n,xlab='',main='Normal pseudo-residuals',freq=F,xlim=c(mi,ma))
   xs=seq(mi,ma,(ma-mi)/1000)
   par(xpd=NA)
-  lines(xs,dnorm(xs))
+  lines(xs,dnorm(xs),lty=3)
   par(xpd=F)
 
-  qqnorm(n,xlim=c(mi,ma),ylim=c(mi,ma))
-  abline(0,1)
+  P <- ppoints(length(n))
+  z=qnorm(P)
+  plot(z, sort(n),xlab='Theoretical quantiles',ylab='Sample quantiles', main = "Normal Q-Q Plot",xlim=c(mi,ma),ylim=c(mi,ma))
+  P1000 = ppoints(1000)
+  z=qnorm(P1000)
+  grid(lty=1)
+  abline(0,1,lty=3)
+  SE <- (1/dnorm(z))*sqrt(P1000*(1 - P1000)/length(n))
+  upper <- z+SE*1.959964
+  lower <- z-SE*1.959964
+  lines(z, upper, lty=3)
+  lines(z, lower, lty=3)
+
   par(old.par)
 }
 
@@ -103,6 +118,7 @@ testResid=function(x) {
     warning('Ignoring branches with zero probability.')
   }
   n=qnorm(p)#normal pseudo-residual
-  r=shapiro.test(n)
-  return(r)
+  r1=shapiro.test(n)#tests if Normal but not if Normal(0,1)
+  r2=ks.test(p,punif,min=0,max=1)#this is same as ks.test(n,pnorm,mean=0,sd=1)
+  if (r1$p.value<r2$p.value) return(r1) else return(r2)
 }
