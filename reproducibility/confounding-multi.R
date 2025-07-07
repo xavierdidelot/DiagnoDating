e51dfa7856f8454d78893974a30a91a4a7e29531
Xavier Didelot <xavier.didelot@gmail.com>
2025-07-07 22:22:22 +0100

    reproducibility

--- reproducibility/confounding-multi.R
+++ reproducibility/confounding-multi.R
@@ -7,17 +7,27 @@ cl <- makeCluster(parallel::detectCores() - 1, type = "PSOCK", outfile="")
 registerDoParallel(cl)
 algos=c('BactDating','treedater','node.dating','TreeTime','LSD')
 
-reps=20
+reps=100
 allres <- foreach (rep = 1:reps,.packages = c('ape','BactDating','DiagnoDating')) %dopar% {
-  set.seed(rep)
-  s=list(runif(50,2010,2010),runif(50,2011,2011),runif(50,2012,2012))
-  dt=simStructure(popStarts=c(2009,2009,2009),samplingDates=s,globalNeg = 100)
-  dates=unname(dist.nodes(dt)[Ntip(dt)+1,1:Ntip(dt)])+dt$root.time
+  cat('Starting task',rep,'\n')
+  capture.output({
+    set.seed(rep)
+    s=list(runif(50,2010,2010),runif(50,2011,2011),runif(50,2012,2012))
+    dt=simStructure(popStarts=c(2009,2009,2009),samplingDates=s,globalNeg = 100)
+    dates=unname(dist.nodes(dt)[Ntip(dt)+1,1:Ntip(dt)])+dt$root.time
 
-  tree=simobsphy(dt,mu=10,model='poisson')
-  l=list(seed=rep,dt=dt)
-  for (i in 1:5) l[[i+2]]=runDating(tree,dates,algo=algos[i])
-  cat('Finished iteration number',rep,'\n')
+    tree=simobsphy(dt,mu=10,model='poisson')
+    l=list(seed=rep,dt=dt)
+    for (i in 1:5) {
+      rd=runDating(tree,dates,algo=algos[i])
+      if (i==1) {
+        rd$p1=ppcheck(rd)
+        rd$p2=median(postdistpvals(rd)$ps)
+      }
+      l=c(l,list(rd))
+    }
+  })
+  cat('Finished task',rep,'\n')
   return(l)
 }
 parallel::stopCluster(cl)
@@ -27,11 +37,11 @@ rm(list=ls())
 load('confounding-multi.RData')
 v=matrix(NA,length(allres),10)
 for (i in 1:length(allres)) {
-    for (j in 1:5) {
-      res=allres[[i]][[j+2]]
-      v[i,1+(j-1)*2  ]=res$rate
-      v[i,1+(j-1)*2+1]=res$rootdate-allres[[i]]$dt$root.time
-    }
+  for (j in 1:5) {
+    res=allres[[i]][[j+2]]
+    v[i,1+(j-1)*2  ]=res$rate
+    v[i,1+(j-1)*2+1]=res$rootdate-allres[[i]]$dt$root.time
+  }
 }
 colnames(v)=sprintf(rep(c('Rate %s','TMRCA %s'),5),rep(algos,each=2))
 pdf('confounding-multi.pdf',10,10)
@@ -44,6 +54,6 @@ system('open confounding-multi.pdf')
 
 allpvals=matrix(NA,length(allres),2)
 for (i in 1:length(allres)) {
-allpvals[i,1]=ppcheck(allres[[i]][[3]])
-allpvals[i,2]=median(postdistpvals(allres[[i]][[3]])$ps)
+  allpvals[i,1]=allres[[i]][[3]]$p1
+  allpvals[i,2]=allres[[i]][[3]]$p2
 }
--- reproducibility/falsePos-multi.R
+++ reproducibility/falsePos-multi.R
@@ -3,31 +3,35 @@ rm(list=ls())
 library(DiagnoDating,quietly=T)
 library(doParallel)
 
-cl <- makeCluster(parallel::detectCores() - 1, type = "PSOCK")
+cl <- makeCluster(parallel::detectCores() - 1, type = "PSOCK", outfile="")
 registerDoParallel(cl)
 algos=c('BactDating','treedater','node.dating','TreeTime','LSD')
 
 reps=100
 allres <- foreach (rep = 1:(4*reps),.packages = c('ape','BactDating','DiagnoDating')) %dopar% {
-  seed=floor(seq(1,reps+0.75,0.25))[rep]
-  set.seed(seed)
-  dates=runif(200,2000,2020)
-  dt=simcoaltree(dates,10)
-  phy=simobsphy(dt,mu=10,model='poisson')
-  r0=resDating(dt,phy)
-  p0=testResid(r0)$p.value
-  if (rep%%4 == 1 || rep%%4 == 2) keepRoot=T else {keepRoot=F;phy=unroot(phy)}
-  if (rep%%4 == 1 || rep%%4 == 3) rate=NA else rate=10
+  cat('Starting task',rep,'\n')
+  capture.output({
+    seed=floor(seq(1,reps+0.75,0.25))[rep]
+    set.seed(seed)
+    dates=runif(200,2000,2020)
+    dt=simcoaltree(dates,10)
+    phy=simobsphy(dt,mu=10,model='poisson')
+    r0=resDating(dt,phy)
+    p0=testResid(r0)$p.value
+    if (rep%%4 == 1 || rep%%4 == 2) keepRoot=T else {keepRoot=F;phy=unroot(phy)}
+    if (rep%%4 == 1 || rep%%4 == 3) rate=NA else rate=10
 
-  l=list(seed=seed,keepRoot=keepRoot,rate=rate,truth=p0)
+    l=list(seed=seed,keepRoot=keepRoot,rate=rate,truth=p0)
 
-  for (j in 1:length(algos)) {
-    r1=runDating(phy,dates,algo=algos[j],keepRoot=keepRoot,rate=rate)
-    p1p=ppcheck(r1)
-    if (j>1) r1=resample(r1)
-    v1=postdistpvals(r1);p1=median(v1$ps)
-    l=c(l,list(p1p,p1))
-  }
+    for (j in 1:length(algos)) {
+      r1=runDating(phy,dates,algo=algos[j],keepRoot=keepRoot,rate=rate)
+      p1p=ppcheck(r1)
+      if (j>1) r1=resample(r1)
+      v1=postdistpvals(r1);p1=median(v1$ps)
+      l=c(l,list(p1p,p1))
+    }
+  })
+  cat('Finished task',rep,'\n')
   return(l)
 }
 
